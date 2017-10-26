714aa5844eb6f67e7245857f286ee9baef279f4b
vlinx <vlinx@vlinx.io>
2017-10-26 20:28:19 +0800

    Fix classpath problem

--- main.go
+++ main.go
@@ -1,35 +1,35 @@
 package main
 
 import (
+	"encoding/json"
+	"github.com/vlinx-io/java-wrapper/settings"
+	"github.com/vlinx-io/java-wrapper/utils"
+	"io/ioutil"
+	"log"
 	"os"
 	"os/exec"
-	"io/ioutil"
-	"github.com/vlinx-io/java-wrapper/settings"
-	"encoding/json"
-	"github.com/vlinx-io/java-wrapper/utils"
 	"runtime"
-	"log"
+	"strings"
 )
 
 // 这个Wrapper这周先做windows的吧，下周再做linux和mac两个系统的
 func main() {
 
-
 	exeDir := utils.GetExeDir()
 
-	settingsFile := exeDir + string(os.PathSeparator) + "settings.json"
+	settingsFile := exeDir + string(os.PathSeparator) + "wrapper.json"
 
 	data, err := ioutil.ReadFile(settingsFile)
 
-	if err!=nil {
+	if err != nil {
 		log.Fatalln(err)
 	}
 
 	var setting settings.Settings
 
-	err = json.Unmarshal(data,&setting)
+	err = json.Unmarshal(data, &setting)
 
-	if err!=nil {
+	if err != nil {
 		log.Fatalln(err)
 	}
 
@@ -47,39 +47,47 @@ func main() {
 	executable := exeDir + string(os.PathSeparator) + "jre" + string(os.PathSeparator) + "bin" + string(os.PathSeparator) + command
 
 	if setting.Verbose {
-		log.Println("Command:",executable)
+		log.Println("Command:", executable)
 	}
 
 	// 将setting中的项都赋值出来，不直接在setting中操作
 	classpath := setting.Classpath
 
 	// 遍历classpath，将每个相对路径都变为绝对路径
-	for index,value := range classpath {
+	for index, value := range classpath {
 
 		value = exeDir + string(os.PathSeparator) + value
 		classpath[index] = value
 
 	}
 
-	classpath = append([]string{"-cp"},classpath...)
+	sep := ":"
+
+	if runtime.GOOS == "windows" {
+		sep = ";"
+	}
+
+	//将classpath转换为字符串
+	cp := strings.Join(classpath,sep)
+
+	classpath = append([]string{"-cp"}, cp)
 
 	// java的Arg在前，classpath在后
-	args := append(setting.JArgs,classpath...)
+	args := append(setting.JArgs, classpath...)
 
 	// 将mainClass内容并入参数
-	args = append(args,setting.MainClass)
+	args = append(args, setting.MainClass)
 
 	// 再将用户命令中的参数赋值
 	if len(os.Args) > 1 {
-		args = append(args,os.Args[1:]...)
+		args = append(args, os.Args[1:]...)
 	}
 
 	if setting.Verbose {
-		log.Println("Args:",args)
+		log.Println("Args:", args)
 	}
 
-
-	process := exec.Command(executable,args...)
+	process := exec.Command(executable, args...)
 
 	process.Stdin = os.Stdin
 	process.Stdout = os.Stdout
@@ -87,7 +95,7 @@ func main() {
 
 	err = process.Run()
 
-	if err!=nil {
+	if err != nil {
 		log.Fatalln(err)
 	}
 
